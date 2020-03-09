d11908b9fc76e95c76d46596461469d042877aaf
thomsyne <oluwatomisinoluwafemi@gmail.com>
2020-03-09 10:04:08 +0000

    Secure API with login and token authentication

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength =4, ErrorMessage = "Passwords must be more than 4 characters")]
+        public string Password { get; set; }
+    }
+}
